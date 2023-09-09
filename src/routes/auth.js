9ca75781ccc37a1407dcac09eca94b18d6d9b16e
swawozny <sebastianwawony@wp.pl>
2023-09-09 14:04:34 +0200

    Move data validation to separate function

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,17 +5,18 @@ const {validateSignUpData, validateEmail, validatePassword} = require("../valida
 const {validateLoginData} = require("../validations/loginData");
 const {validateConfirmEmailData} = require("../validations/confirmData");
 const {validateResetPasswordData} = require("../validations/resetPasswordData");
+const {checkValidationErrors} = require("../validations/checkValidationErrors");
 
 const router = express.Router();
 
-router.post('/signup', validateSignUpData, authController.signUp);
+router.post('/signup', validateSignUpData, checkValidationErrors, authController.signUp);
 
-router.post('/login', validateLoginData, authController.login);
+router.post('/login', validateLoginData, checkValidationErrors, authController.login);
 
-router.post('/confirm-email', validateConfirmEmailData, authController.confirmEmail);
+router.post('/confirm-email', validateConfirmEmailData, checkValidationErrors, authController.confirmEmail);
 
-router.post('/try-reset-password', validateEmail, authController.tryResetPassword);
+router.post('/try-reset-password', validateEmail, checkValidationErrors, authController.tryResetPassword);
 
-router.post('/reset-password', validateResetPasswordData, authController.resetPassword);
+router.post('/reset-password', validateResetPasswordData, checkValidationErrors, authController.resetPassword);
 
 module.exports = router;
