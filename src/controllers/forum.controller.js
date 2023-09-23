835c6d1c580fbbee5d734dcc50ed00ad8c4b485e
swawozny <sebastianwawony@wp.pl>
2023-09-23 21:39:51 +0200

    Add DELETE forum endpoint

--- src/controllers/forum.controller.js
+++ src/controllers/forum.controller.js
@@ -1,5 +1,7 @@
-const {Forum} = require('../database/models');
 const {StatusCodes} = require("http-status-codes");
+
+const {Forum} = require("../database/models");
+const ApiError = require("../errors/apiError");
 
 exports.getForums = async (req, res, next) => {
     try {
@@ -19,6 +21,28 @@ exports.getForums = async (req, res, next) => {
         return res
             .status(StatusCodes.OK)
             .json({forums});
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+        }
+        next(error);
+    }
+};
+
+exports.deleteForum = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+
+        const forum = await Forum.findByPk(id);
+
+        if (!forum) {
+            throw new ApiError(StatusCodes.NOT_FOUND, 'Forum id is not correct!');
+        }
+
+        await forum.destroy();
+
+        return res.status(StatusCodes.OK).json({message: 'Forum deleted.'})
+
     } catch (error) {
         if (!error.statusCode) {
             error.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
