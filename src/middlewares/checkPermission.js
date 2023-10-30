1f8bfb7868efb0c7c464d87c2dcdec721c082253
swawozny <sebastianwawony@wp.pl>
2023-10-30 18:21:43 +0100

    Fix change permission middleware

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -3,43 +3,34 @@ const {StatusCodes} = require("http-status-codes");
 const {User, Permission, Role} = require("../database/models");
 const ApiError = require("../errors/apiError");
 
-module.exports = (permissionToCheck) => async (req, res, next) => {
-    try {
-        const user = await User.findByPk(req.userId, {
-            include: {
-                model: Role,
-                as: "userRole"
-            }
-        });
-
-        if (!user) {
-            throw new ApiError(StatusCodes.UNAUTHORIZED, "User invalid!");
+module.exports = async (userId, permissionToCheck) => {
+    const user = await User.findByPk(userId, {
+        include: {
+            model: Role,
+            as: "userRole"
         }
+    });
 
-        const attributes = ["name"];
-        const {rolePermissions} = await Role.findOne({
-            where: {
-                id: user.roleId,
-            },
-            attributes,
-            include: {
-                model: Permission,
-                as: "rolePermissions",
-                attributes
-            },
-        });
-
-        const isAuthorized = rolePermissions.find(permission => permission.name === permissionToCheck);
-
-        if (!isAuthorized) {
-            throw new ApiError(StatusCodes.UNAUTHORIZED, "User does not have right permissions!");
-        }
-    } catch (error) {
-        if (!error.statusCode) {
-            error.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
-        }
-        next(error);
+    if (!user) {
+        throw new ApiError(StatusCodes.UNAUTHORIZED, "User invalid!");
     }
 
-    next();
+    const attributes = ["name"];
+    const {rolePermissions} = await Role.findOne({
+        where: {
+            id: user.roleId,
+        },
+        attributes,
+        include: {
+            model: Permission,
+            as: "rolePermissions",
+            attributes
+        },
+    });
+
+    const isAuthorized = rolePermissions.some(permission => permission.name === permissionToCheck);
+
+    if (!isAuthorized) {
+        throw new ApiError(StatusCodes.UNAUTHORIZED, "User does not have right permissions!");
+    }
 };
